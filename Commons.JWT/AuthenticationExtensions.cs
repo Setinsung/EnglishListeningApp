35103e9dc6a658b846fc00f93cc58253202c320f
Setinsung <1968989251@qq.com>
2023-10-08 20:08:51 +0800

    Commons.JWT

---
+++ Commons.JWT/AuthenticationExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace Commons.JWT;
+
+public static class AuthenticationExtensions
+{
+    /// <summary>
+    /// 配置JWT
+    /// </summary>
+    /// <param name="services"></param>
+    /// <param name="jwtOpt"></param>
+    /// <returns></returns>
+    public static AuthenticationBuilder AddJWTAuthentication(this IServiceCollection services, JWTOptions jwtOpt)
+    {
+        return services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(opt =>
+            {
+                opt.TokenValidationParameters = new()
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = jwtOpt.Issuer,
+                    ValidAudience = jwtOpt.Audience,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtOpt.Key))
+                };
+            });
+    }
+}
---
+++ Commons.JWT/TokenService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Commons.JWT;
+
+public class TokenService : ITokenService
+{
+    public string BuildToken(IEnumerable<Claim> claims, JWTOptions options)
+    {
+        TimeSpan ExpiryDuration = TimeSpan.FromSeconds(options.ExpireSeconds);
+        var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(options.Key));
+        var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256Signature);
+        var tokenDescriptor = new JwtSecurityToken(
+            issuer: options.Issuer,
+            audience: options.Audience,
+            claims: claims,
+            expires: DateTime.Now.Add(ExpiryDuration),
+            signingCredentials: credentials
+        );
+        return new JwtSecurityTokenHandler().WriteToken(tokenDescriptor);
+    }
+}
