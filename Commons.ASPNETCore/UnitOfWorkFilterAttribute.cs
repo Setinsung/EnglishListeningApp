f5854ac00d5075ffab9bbdac35e2e73a5a4ee7c8
Setinsung <1968989251@qq.com>
2023-10-09 20:37:31 +0800

    UnitOfWorkFilterAttribute

---
+++ Commons.ASPNETCore/UnitOfWorkFilterAttribute.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Commons.ASPNETCore;
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+public class UnitOfWorkFilterAttribute : ActionFilterAttribute
+{
+    public Type[] DbContextTypes { get; set; }
+    public UnitOfWorkFilterAttribute(params Type[] dbContextTypes)
+    {
+        DbContextTypes = dbContextTypes;
+    }
+    public override async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+    {
+        var result = await next();
+        if (result.Exception != null) return;
+        foreach (var dbCtxType in DbContextTypes)
+        {
+            var sp = context.HttpContext.RequestServices;
+            DbContext dbCtx = (DbContext)sp.GetRequiredService(dbCtxType);
+            await dbCtx.SaveChangesAsync();
+        }
+    }
+}
