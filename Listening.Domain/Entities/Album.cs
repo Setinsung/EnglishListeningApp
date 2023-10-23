3ee07161b07f3d8d7314a261510b46fe907b1f14
Setinsung <1968989251@qq.com>
2023-10-24 02:05:52 +0800

    Listening.Domain Alnum Category

---
+++ Listening.Domain/Entities/Album.cs
@@ -0,0 +1,64 @@
+﻿using Commons.Domain.Models;
+
+namespace Listening.Domain.Entities;
+
+public record Album : AggregateRootEntity
+{
+    private Album() { }
+
+    /// <summary>
+    /// 用户是否可见（完善后才显示）
+    /// </summary>
+    public bool IsVisible { get; private set; }
+
+    /// <summary>
+    /// 标题
+    /// </summary>
+    public MultilingualString Name { get; private set; }
+
+    /// <summary>
+    /// 列表显示序号
+    /// </summary>
+    public int SequenceNumber { get; private set; }
+
+    /// <summary>
+    /// 类别
+    /// </summary>
+    public Guid CategoryId { get; private set; }
+
+    public static Album Create(Guid id, int sequenceNumber, MultilingualString name, Guid categoryId)
+    {
+        return new Album()
+        {
+            Id = id,
+            Name = name,
+            SequenceNumber = sequenceNumber,
+            CategoryId = categoryId,
+            IsVisible = false // Album新建以后默认不可见，需要手动Show
+
+        };
+    }
+
+    public Album ChangeSequenceNumber(int value)
+    {
+        this.SequenceNumber = value;
+        return this;
+    }
+
+    public Album ChangeName(MultilingualString value)
+    {
+        this.Name = value;
+        return this;
+    }
+
+    public Album Hide()
+    {
+        this.IsVisible = false;
+        return this;
+    }
+    public Album Show()
+    {
+        this.IsVisible = true;
+        return this;
+    }
+}
---
+++ Listening.Domain/Entities/Category.cs
@@ -0,0 +1,54 @@
+﻿using Commons.Domain.Models;
+
+namespace Listening.Domain.Entities;
+
+public record Category : AggregateRootEntity
+{
+    private Category() { }
+
+    /// <summary>
+    /// 类别名称
+    /// </summary>
+    public MultilingualString Name { get; private set; }
+
+    /// <summary>
+    /// 显示序号，靠前优先
+    /// </summary>
+    public int SequenceNumber { get; private set; }
+
+    /// <summary>
+    /// 封面图片路径
+    /// </summary>
+    public Uri CoverUrl { get; private set; }
+
+    public static Category Create(Guid id, int sequenceNumber, MultilingualString name, Uri coverUrl)
+    {
+        var category = new Category()
+        {
+            Id = id,
+            Name = name,
+            SequenceNumber = sequenceNumber,
+            CoverUrl = coverUrl
+        };
+        // category.AddDomainEvent(new CategoryCreatedEventArgs { NewObj = category });
+        return category;
+    }
+    
+    public Category ChangeName(MultilingualString value)
+    {
+        this.Name = value;
+        return this;
+    }
+    public Category ChangeSequenceNumber(int value)
+    {
+        this.SequenceNumber = value;
+        return this;
+    }
+
+    public Category ChangeCoverUrl(Uri coverUrl)
+    {
+        // 实际应该不管这个事件是否有被用到，都应尽量publish。
+        this.CoverUrl = coverUrl;
+        return this;
+    }
+}
