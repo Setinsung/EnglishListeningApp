b3bd943adf896d9b99e35217a780a91ebd19bd29
Setinsung <1968989251@qq.com>
2023-11-01 01:32:52 +0800

    Add Document Comment

--- Listening.Domain/ListeningDomainService.cs
+++ Listening.Domain/ListeningDomainService.cs
@@ -13,6 +13,12 @@ public class ListeningDomainService
         this._listeningRepository = listeningRepository;
     }
 
+    /// <summary>
+    /// 添加一个专辑到指定分类下
+    /// </summary>
+    /// <param name="categoryId">分类ID</param>
+    /// <param name="name">专辑名称</param>
+    /// <returns>新添加的专辑</returns>
     public async Task<Album?> AddAlbumAsync(Guid categoryId, MultilingualString name)
     {
         if (await _listeningRepository.GetCategoryByIdAsync(categoryId) == null) return null;
@@ -22,6 +28,11 @@ public class ListeningDomainService
         return album;
     }
 
+    /// <summary>
+    /// 对指定分类下的专辑进行排序
+    /// </summary>
+    /// <param name="categoryId">分类ID</param>
+    /// <param name="sortedAlbumIds">排序后的专辑ID列表</param>
     public async Task SortAlbumsAsync(Guid categoryId, IEnumerable<Guid> sortedAlbumIds)
     {
         IEnumerable<Album> albums = await _listeningRepository.GetAlbumsByCategoryIdAsync(categoryId);
@@ -36,6 +47,12 @@ public class ListeningDomainService
         }
     }
 
+    /// <summary>
+    /// 添加一个分类
+    /// </summary>
+    /// <param name="name">分类名称</param>
+    /// <param name="coverUrl">分类封面URL</param>
+    /// <returns>新添加的分类</returns>
     public async Task<Category> AddCategoryAsync(MultilingualString name, Uri coverUrl)
     {
         int maxSeq = await _listeningRepository.GetMaxSeqOfCategoriesAsync();
@@ -44,6 +61,10 @@ public class ListeningDomainService
         return category;
     }
 
+    /// <summary>
+    /// 对所有分类进行排序
+    /// </summary>
+    /// <param name="sortedCategoryIds">排序后的分类ID列表</param>
     public async Task SortCategoriesAsync(IEnumerable<Guid> sortedCategoryIds)
     {
         IEnumerable<Category> categories = await _listeningRepository.GetCategoriesAsync();
@@ -58,6 +79,16 @@ public class ListeningDomainService
         }
     }
 
+    /// <summary>
+    /// 添加一个音频到指定专辑下
+    /// </summary>
+    /// <param name="name">音频名称</param>
+    /// <param name="albumId">专辑ID</param>
+    /// <param name="audioUrl">音频URL</param>
+    /// <param name="durationInSecond">音频时长（秒）</param>
+    /// <param name="subtitleType">字幕类型</param>
+    /// <param name="subtitle">字幕</param>
+    /// <returns>新添加的音频</returns>
     public async Task<Episode?> AddEpisodeAsync(MultilingualString name, Guid albumId, Uri audioUrl, double durationInSecond, string subtitleType, string subtitle)
     {
         if (await _listeningRepository.GetAlbumByIdAsync(albumId) == null) return null;
@@ -71,6 +102,11 @@ public class ListeningDomainService
         return episode;
     }
 
+    /// <summary>
+    /// 对指定专辑下的音频进行排序
+    /// </summary>
+    /// <param name="albumId">专辑ID</param>
+    /// <param name="sortedEpisodeIds">排序后的音频ID列表</param>
     public async Task SortEpisodesAsync(Guid albumId, IEnumerable<Guid> sortedEpisodeIds)
     {
         IEnumerable<Episode> episodes = await _listeningRepository.GetEpisodesByAlbumIdAsync(albumId);
