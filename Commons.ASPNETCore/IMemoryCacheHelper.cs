d6ef6a6ec03ab9891eb33d1b99a4309a8c1487f3
Setinsung <1968989251@qq.com>
2023-11-02 23:47:39 +0800

    MemoryCacheHepler modify

--- Commons.ASPNETCore/IMemoryCacheHelper.cs
+++ Commons.ASPNETCore/IMemoryCacheHelper.cs
@@ -3,29 +3,29 @@
 namespace Commons.ASPNETCore;
 
 /// <summary>
-/// 内存缓存帮助类接口
+/// 内存缓存帮助类接口，验证不允许是IEnumerable和IQueryable，避开延迟加载。同时设置过期时间为expireSeconds至两倍expireSeconds之间随机偏移
 /// </summary>
 public interface IMemoryCacheHelper
 {
     /// <summary>
     /// 获取或创建指定缓存键的值
     /// </summary>
-    /// <typeparam name="T">值的类型</typeparam>
+    /// <typeparam name="TItem">值的类型</typeparam>
     /// <param name="cacheKey">缓存键</param>
     /// <param name="valueFactory">值的创建方法</param>
     /// <param name="expireSeconds">缓存过期时间（以秒为单位），默认为60秒</param>
     /// <returns>缓存值</returns>
-    T? GetOrCreate<T>(string cacheKey, Func<ICacheEntry, T?> valueFactory, int expireSeconds = 60);
+    TItem? GetOrCreate<TItem>(string cacheKey, Func<ICacheEntry, TItem> valueFactory, int expireSeconds = 60);
 
     /// <summary>
     /// 异步获取或创建指定缓存键的值
     /// </summary>
-    /// <typeparam name="T">值的类型</typeparam>
+    /// <typeparam name="TItem">值的类型</typeparam>
     /// <param name="cacheKey">缓存键</param>
     /// <param name="valueFactory">值的创建方法</param>
     /// <param name="expireSeconds">缓存过期时间（以秒为单位），默认为60秒</param>
     /// <returns>缓存值</returns>
-    Task<T?> GetOrCreateAsync<T>(string cacheKey, Func<ICacheEntry, Task<T?>> valueFactory, int expireSeconds = 60);
+    Task<TItem?> GetOrCreateAsync<TItem>(string cacheKey, Func<ICacheEntry, Task<TItem>> valueFactory, int expireSeconds = 60);
 
 
     /// <summary>
--- Commons.ASPNETCore/MemoryCacheHelper.cs
+++ Commons.ASPNETCore/MemoryCacheHelper.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.Extensions.Caching.Memory;
-using System.Collections;
 
 namespace Commons.ASPNETCore;
 
@@ -11,27 +10,27 @@ public class MemoryCacheHelper : IMemoryCacheHelper
     {
         this._memoryCache = memoryCache;
     }
-    public T? GetOrCreate<T>(string cacheKey, Func<ICacheEntry, T?> valueFactory, int expireSeconds = 60)
+    public TItem? GetOrCreate<TItem>(string cacheKey, Func<ICacheEntry, TItem> valueFactory, int expireSeconds = 60)
     {
-        ValidateValueType<T>();
-        if (!_memoryCache.TryGetValue(cacheKey, out T? result))
+        ValidateValueType<TItem>();
+        if (!_memoryCache.TryGetValue(cacheKey, out TItem? result))
         {
             using ICacheEntry entry = _memoryCache.CreateEntry(cacheKey);
             InitCacheEntry(entry, expireSeconds);
-            result = valueFactory(entry)!;
+            result = valueFactory(entry);
             entry.Value = result;
         }
         return result;
     }
 
-    public async Task<T?> GetOrCreateAsync<T>(string cacheKey, Func<ICacheEntry, Task<T?>> valueFactory, int expireSeconds = 60)
+    public async Task<TItem?> GetOrCreateAsync<TItem>(string cacheKey, Func<ICacheEntry, Task<TItem>> valueFactory, int expireSeconds = 60)
     {
-        ValidateValueType<T>();
-        if (!_memoryCache.TryGetValue(cacheKey, out T? result))
+        ValidateValueType<TItem>();
+        if (!_memoryCache.TryGetValue(cacheKey, out TItem? result))
         {
             using ICacheEntry entry = _memoryCache.CreateEntry(cacheKey);
             InitCacheEntry(entry, expireSeconds);
-            result = (await valueFactory(entry))!;
+            result = await valueFactory(entry).ConfigureAwait(false);
             entry.Value = result;
         }
         return result;
@@ -42,6 +41,9 @@ public class MemoryCacheHelper : IMemoryCacheHelper
         _memoryCache.Remove(cacheKey);
     }
 
+    /// <summary>
+    /// 验证类型。不允许是IEnumerable和IQueryable，避开延迟加载。
+    /// </summary>
     private static void ValidateValueType<T>()
     {
         Type typeResult = typeof(T);
@@ -50,15 +52,17 @@ public class MemoryCacheHelper : IMemoryCacheHelper
             typeResult = typeResult.GetGenericTypeDefinition(); // 获取泛型定义类型
         }
         if (typeResult == typeof(IEnumerable<>) ||
-            typeResult == typeof(IEnumerable) ||
-            typeResult == typeof(IAsyncEnumerable<T>) ||
-            typeResult == typeof(IQueryable<T>) ||
-            typeResult == typeof(IQueryable))
+            typeResult == typeof(IAsyncEnumerable<>) ||
+            typeResult == typeof(IQueryable<>))
         {
             throw new InvalidOperationException($"T of {typeResult} is not allowed, please use List<T> or T[] instead.");
         }
     }
-
+    /// <summary>
+    /// 初始化缓存条目entry，设置过期时间为expireSeconds至两倍expireSeconds之间随机偏移。
+    /// </summary>
+    /// <param name="entry">缓存条目。</param>
+    /// <param name="expireSeconds">过期时间（秒）。</param>
     private static void InitCacheEntry(ICacheEntry entry, int expireSeconds)
     {
         double sec = Random.Shared.NextDouble(expireSeconds, expireSeconds * 2);
