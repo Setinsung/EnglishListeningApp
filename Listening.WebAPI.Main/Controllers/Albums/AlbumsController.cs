7450e9b8c0b3c07f572ef693c6a918e0a5ba0b3d
Setinsung <1968989251@qq.com>
2023-11-03 15:13:13 +0800

    Listening.WebAPI.Main AlbumsController

--- Listening.WebAPI.Main/Controllers/Albums/AlbumsController.cs
+++ Listening.WebAPI.Main/Controllers/Albums/AlbumsController.cs
@@ -1,10 +1,44 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Commons.ASPNETCore;
+using Commons.Helpers.Validators;
+using Listening.Domain;
+using Listening.WebAPI.Main.Controllers.Albums.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace Listening.WebAPI.Main.Controllers.Albums;
 
-[Route("[controller]")]
+[Route("api/[controller]")]
 [ApiController]
 public class AlbumsController : ControllerBase
 {
+    private readonly IListeningRepository _listeningRepository;
+    private readonly IMemoryCache _memoryCache;
 
+    public AlbumsController(IListeningRepository listeningRepository, IMemoryCache memoryCache)
+    {
+        this._listeningRepository = listeningRepository;
+        this._memoryCache = memoryCache;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<AlbumVM>> FindById([RequiredGuid] Guid id)
+    {
+        var album = await _memoryCache.GetOrCreateWithRandomExpiryAsync(
+            $"AlbumsController.FindById.{id}",
+            async (e) => AlbumVM.Create(await _listeningRepository.GetAlbumByIdAsync(id))
+        );
+        if(album == null) return NotFound("专辑不存在");
+        return Ok(album);
+    }
+
+    [HttpGet("/list")]
+    public async Task<ActionResult<IEnumerable<AlbumVM>>> FindByCategoryId([RequiredGuid] [FromQuery] Guid categoryId)
+    {
+        var albums = await _memoryCache.GetOrCreateWithRandomExpiryAsync(
+            $"AlbumsController.FindByCategoryId.{categoryId}",
+            async (e) => AlbumVM.Create(await _listeningRepository.GetAlbumsByCategoryIdAsync(categoryId))
+        );
+        if (albums == null) return NotFound("专辑不存在");
+        return Ok(albums);
+    }
 }
---
+++ Listening.WebAPI.Main/Controllers/Albums/ViewModels/AlbumVM.cs
@@ -0,0 +1,18 @@
+﻿using Commons.Domain.Models;
+using Listening.Domain.Entities;
+
+namespace Listening.WebAPI.Main.Controllers.Albums.ViewModels;
+
+public record AlbumVM(Guid Id, MultilingualString Name, Guid CategoryId)
+{
+    public static AlbumVM? Create(Album? album)
+    {
+        if (album == null) return null;
+        return new AlbumVM(album.Id, album.Name, album.CategoryId);
+    }
+
+    public static AlbumVM?[] Create(Album[] albums)
+    {
+        return albums.Select(Create).ToArray();
+    }
+}
