bb1dec584a9cf284731f0d4b24b7768614005fd6
Setinsung <1968989251@qq.com>
2023-11-09 00:19:12 +0800

    MediaEncoder.Infrastructure

---
+++ MediaEncoder.Infrastructure/M4AEncoder.cs
@@ -0,0 +1,28 @@
+﻿using FFmpeg.NET;
+using MediaEncoder.Domain;
+
+namespace MediaEncoder.Infrastructure;
+
+public class M4AEncoder : IMediaEncoder
+{
+    public bool Accept(string outputFormat)
+    {
+        return "m4a".Equals(outputFormat, StringComparison.OrdinalIgnoreCase);
+    }
+
+    public async Task EncodeAsync(FileInfo sourceFile, FileInfo destFile, string destFormat, string[]? args, CancellationToken cancellationToken)
+    {
+        InputFile inputFile = new(sourceFile);
+        OutputFile outputFile = new(destFile);
+        string baseDir = AppContext.BaseDirectory;
+        string ffmpegPath = Path.Combine(baseDir, "ffmpeg.exe");
+        Engine ffmpeg = new(ffmpegPath);
+        string? errorMsg = null;
+        ffmpeg.Error += (o, e) =>
+        {
+            errorMsg = e.Exception.Message;
+        };
+        await ffmpeg.ConvertAsync(inputFile, outputFile,cancellationToken).ConfigureAwait(false);
+        if(errorMsg != null) throw new Exception(errorMsg);
+    }
+}
---
+++ MediaEncoder.Infrastructure/ModuleInitializer.cs
@@ -0,0 +1,14 @@
+﻿using Commons.Helpers;
+using MediaEncoder.Domain;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MediaEncoder.Infrastructure;
+
+public class ModuleInitializer : IModuleInitializer
+{
+    public void Initialize(IServiceCollection services)
+    {
+        services.AddScoped<IMediaEncoderRepository, MediaEncoderRepository>();
+        services.AddScoped<IMediaEncoder, M4AEncoder>();
+    }
+}
