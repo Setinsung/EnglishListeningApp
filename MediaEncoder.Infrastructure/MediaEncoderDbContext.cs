037dc2d51574da828929dcef26fd1f46656b29ab
Setinsung <1968989251@qq.com>
2023-11-08 23:46:02 +0800

    MediaEncoder.Infrastructure Repository

---
+++ MediaEncoder.Infrastructure/MediaEncoderDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Commons.Infrastructure;
+using MediaEncoder.Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace MediaEncoder.Infrastructure;
+
+public class MediaEncoderDbContext : BaseDbContext
+{
+    public DbSet<EncodingItem> EncodingItems { get; private set; }
+
+    public MediaEncoderDbContext(DbContextOptions options, IMediator? mediator) : base(options, mediator)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(this.GetType().Assembly);  
+        modelBuilder.EnableSoftDeletionGlobalFilter();
+    }
+}
---
+++ MediaEncoder.Infrastructure/MediaEncoderRepository.cs
@@ -0,0 +1,24 @@
+﻿using MediaEncoder.Domain;
+using MediaEncoder.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace MediaEncoder.Infrastructure;
+
+public class MediaEncoderRepository : IMediaEncoderRepository
+{
+    private readonly MediaEncoderDbContext _mediaEncoderDbContext;
+
+    public MediaEncoderRepository(MediaEncoderDbContext mediaEncoderDbContext)
+    {
+        this._mediaEncoderDbContext = mediaEncoderDbContext;
+    }
+    public Task<EncodingItem[]> FindAsync(ItemStatus status)
+    {
+        return _mediaEncoderDbContext.EncodingItems.Where(e => e.Status == ItemStatus.Ready).ToArrayAsync();
+    }
+
+    public Task<EncodingItem?> FindCompletedOneAsync(string fileHash, long fileSize)
+    {
+        return _mediaEncoderDbContext.EncodingItems.FirstOrDefaultAsync(e => e.FileSHA256Hash == fileHash && e.FileSizeInBytes == fileSize && e.Status == ItemStatus.Completed);
+    }
+}
